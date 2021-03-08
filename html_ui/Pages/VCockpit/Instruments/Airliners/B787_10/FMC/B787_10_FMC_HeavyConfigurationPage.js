3561de2a94f162fee0895be7714addc7b2c918c5
김라댜 <43503767+kimlada@users.noreply.github.com>
2021-03-08 09:44:36 +0100

    Fuel Pumps FIX

--- html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyConfigurationPage.js
+++ html_ui/Pages/VCockpit/Instruments/Airliners/B787_10/FMC/B787_10_FMC_HeavyConfigurationPage.js
@@ -6,7 +6,7 @@ class B787_10_FMC_HeavyConfigurationPage {
 		fmc.setTemplate([
 			['HEAVY CONFIGURATION'],
 			[''],
-			[''],
+			['', 'FUEL PUMPS ON'],
 			[''],
 			[''],
 			[''],
@@ -23,6 +23,17 @@ class B787_10_FMC_HeavyConfigurationPage {
 		fmc.onLeftInput[5] = () => {
 			B787_10_FMC_HeavyPage.ShowPage1(fmc);
 		}
+
+		fmc.onRightInput[0] = () => {
+			SimVar.SetSimVarValue('K:FUELSYSTEM_PUMP_ON', 'Number', 1);
+			SimVar.SetSimVarValue('K:FUELSYSTEM_PUMP_ON', 'Number', 2);
+			SimVar.SetSimVarValue('K:FUELSYSTEM_PUMP_ON', 'Number', 3);
+			SimVar.SetSimVarValue('K:FUELSYSTEM_PUMP_ON', 'Number', 4);
+			SimVar.SetSimVarValue('K:FUELSYSTEM_PUMP_ON', 'Number', 5);
+			SimVar.SetSimVarValue('K:FUELSYSTEM_PUMP_ON', 'Number', 6);
+			B787_10_FMC_HeavyConfigurationPage.ShowPage1(fmc);
+		}
+
 
 		fmc.updateSideButtonActiveStatus();
 	}
